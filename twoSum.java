1b55bd929942c8911db38794d621e8716bd34b57
Jyoti Kumari <jk1076387@gmail.com>
2026-02-18 12:00:05 +0530

    Enhance comments with example input and output
    Added example input and output comments for clarity.

---
+++ twoSum.java
@@ -0,0 +1,37 @@
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order.
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int nums[]=new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        int target =sc.nextInt();
+        Solution obj=new Solution();
+        obj.twoSum(nums,target);
+    }
+}
+
+Output:-
+  [2,7,11,15]
+  9
+  [0,1]
