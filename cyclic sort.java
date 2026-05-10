60b441e732bb3b7f641fa259a4308d2d45ca1d4c
Jyoti Kumari <jk1076387@gmail.com>
2026-05-10 23:33:53 +0530

    Update cyclic sort.java

--- cyclic sort.java
+++ cyclic sort.java
@@ -25,6 +25,32 @@ class Solution {
         nums[scnd]=temp;
     }
 }
+ class Solution {
+    public int singleNumber(int[] nums) {
+       /* int result=0;
+        for(int i=0;i<nums.length;i++){
+            result=result^nums[i];
+        }
+        return result;
+        */
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j])
+                    count++;
+            }
+        if(count ==1){
+            return nums[i];
+        }
+      }
+      return -1;
+    }
+    public static void main(String args[]){
+        int nums[]={2,4,2};
+        Solution obj=new Solution();
+        obj.singleNumber(nums);
+    }
+}
 
 // Find the Duplicate Number
 class Solution {
