4cb33422ba93e78572ea10edba49397fcb9881ea
Jyoti Kumari <jk1076387@gmail.com>
2026-06-19 17:37:11 +0530

    Update searchRange.java

--- searchRange.java
+++ searchRange.java
@@ -8,17 +8,61 @@ Output: [3,4]
 import java.util.*;
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first=-1,last=-1;
-        for(int i=0;i<nums.length;i++){
+    /* int first=-1,last=-1;
+       for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
                 if(first==-1){
                     first=i;
                 }
                 last=i;
             }
-        }
-        return new int[]{first,last};
+        }*/
+    int first=firstOccurrence(nums,target);
+    if(first==-1){ 
+        return new int[]{-1,-1};
     }
+    int last=lastOccurrence(nums, target);
+    return new int[]{first,last};
+    }
+     int firstOccurrence(int nums[],int target){
+        int first=-1;
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                first=mid;
+                end=mid-1;
+            }
+           else if(nums[mid]<target){
+            start=mid+1;
+           }
+           else{
+              end=mid-1;
+           }
+        }
+        return first;
+    }
+     int lastOccurrence(int nums[],int target){
+        int last=-1;
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                last=mid;
+                start=mid+1;
+            }
+           else if(nums[mid]<target){
+            start=mid+1;
+           }
+           else{
+              end=mid-1;
+           }
+        }
+        return last;
+    }
+    
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
